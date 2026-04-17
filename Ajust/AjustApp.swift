be8a781e44eef47b37b4824949fa369b4995b803
Andreiia <aguiar.silva.andreia@gmail.com>
2026-04-16 21:05:51 -0600

    Initial Commit

---
+++ Ajust/AjustApp.swift
@@ -0,0 +1,17 @@
+//
+//  AjustApp.swift
+//  Ajust
+//
+//  Created by Andreia Silva Aguiar on 2026-04-16.
+//
+
+import SwiftUI
+
+@main
+struct AjustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AjustTests/AjustTests.swift
@@ -0,0 +1,17 @@
+//
+//  AjustTests.swift
+//  AjustTests
+//
+//  Created by Andreia Silva Aguiar on 2026-04-16.
+//
+
+import Testing
+@testable import Ajust
+
+struct AjustTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

c0ce19f9baa68de4c9efd877e4b8b05a6c24779d
Andreiia <aguiar.silva.andreia@gmail.com>
2026-04-18 09:56:37 -0600

    feat: add Data Management category and State/Binding project

---
+++ Data-Management-Studies/StateBindingStudy/StateBindingStudy/StateBindingStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateBindingStudyApp.swift
+//  StateBindingStudy
+//
+//  Created by Andreia Silva Aguiar on 2026-04-18.
+//
+
+import SwiftUI
+
+@main
+struct StateBindingStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Data-Management-Studies/StateBindingStudy/StateBindingStudyTests/StateBindingStudyTests.swift
@@ -0,0 +1,17 @@
+//
+//  StateBindingStudyTests.swift
+//  StateBindingStudyTests
+//
+//  Created by Andreia Silva Aguiar on 2026-04-18.
+//
+
+import Testing
+@testable import StateBindingStudy
+
+struct StateBindingStudyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

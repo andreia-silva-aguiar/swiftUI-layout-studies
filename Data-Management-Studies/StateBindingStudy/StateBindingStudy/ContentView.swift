36ac3611fd7ab63dc7dd907b489ba58cb05eceaf
Andreiia <aguiar.silva.andreia@gmail.com>
2026-04-19 11:23:46 -0600

    feat: initial commit for State and Binding study

---
+++ Data-Management-Studies/StateBindingStudy/StateBindingStudy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StateBindingStudy
+//
+//  Created by Andreia Silva Aguiar on 2026-04-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
